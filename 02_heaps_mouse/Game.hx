8451d35ccbe69634fd02f481c1586d239cb898d5
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-05-29 17:30:09 +0100

    translated second example

--- 02_heaps_mouse/Game.hx
+++ 02_heaps_mouse/Game.hx
@@ -1,42 +1,32 @@
-class Game extends hxd.App
-{
-    static function main()
-    {
-        new Game();
-    }
+class Game extends hxd.App {
+	static function main() {
+		new Game();
+	}
 
-    var b:h2d.Bitmap;
+	var b:h2d.Bitmap;
 
-    override function init() 
-    {
-        b = new h2d.Bitmap(h2d.Tile.fromColor(0xff0000, 60, 60), s2d);
-        b.x = 50;
-        b.y = 100;
-        // центрируем тайл (задаем ему якорную точку),
-        // таким образом все трансформации объекта на экране 
-        // будут рассчитываться относительно центра тайла.
-        // Если данный метод не вызывать, то тайл будет трансформироваться
-        // относительно его левой верхней точки
-        b.tile = b.tile.center();
-        b.rotation = Math.PI / 4; // повороты задаются в радианах
+	override function init() {
+		b = new h2d.Bitmap(h2d.Tile.fromColor(0xff0000, 60, 60), s2d);
+		b.x = 50;
+		b.y = 100;
+		b.tile = b.tile.center();
+		b.rotation = Math.PI / 4;
 
-        // Интерактивный объект для обработки событий мыши
-        // По умолчанию он задает прямоугольную область.
-        // Добавляем этот объект как дочерний, 
-        // чтобы он обрабатывал события для родительского объекта b
-        var i = new h2d.Interactive(b.tile.width, b.tile.height, b);
-        // необходимо сместить интерактивный объект для учета его якорной точки,
-        // которая находится посередине тайла
-        i.x = -0.5 * b.tile.width;
-        i.y = -0.5 * b.tile.height;
+		// Interactive object will handle mouse events. By default, they will be a rectangular area.
+		// The third parameter is the object they will be attached to.
+		// In this example i will handle mouse events for our tile b.
+		var i = new h2d.Interactive(b.tile.width, b.tile.height, b);
+        // since we centered the tile anchor, we would need to compensate aligning the Interactive
+        // object the same amount.
+		i.x = -0.5 * b.tile.width;
+		i.y = -0.5 * b.tile.height;
 
-        // Обработчики событий
-        i.onOver = function(_) b.alpha = 0.5;
-        i.onOut = function(_) b.alpha = 1.0;
-    }
+		// Setting a couple of event hanlders
+		i.onOver = function(_) b.alpha = 0.5;
+		i.onOut = function(_) b.alpha = 1.0;
+	}
 
-    override function update(dt:Float) 
-    {
-        b.rotation += 0.01;
-    }
+	override function update(dt:Float) {
+		b.rotation += 0.01;
+	}
 }
