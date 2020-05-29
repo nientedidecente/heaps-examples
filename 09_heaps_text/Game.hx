f6618c0b9516926ea86391f5a372269374fe8ad7
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-05-29 22:54:52 +0100

    translated example 9

--- 09_heaps_text/Game.hx
+++ 09_heaps_text/Game.hx
@@ -1,26 +1,31 @@
-class Game extends hxd.App
-{
-    static function main()
-    {
-        hxd.Res.initLocal();
-        
-        new Game();
-    }
+class Game extends hxd.App {
+	static function main() {
+		hxd.Res.initLocal();
 
-    override function init() 
-    {
-        var t = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
-        t.scale(10);
-        t.text = "Haxe Rocks!!!";
+		new Game();
+	}
 
-        var t = new h2d.HtmlText(hxd.res.DefaultFont.get(), s2d);
-        t.scale(10);
-        t.y = 100;
-        t.text = "Haxe <font color='#ff0000'>Rocks!!!</font>";
+	override function init() {
+        // Heaps provides Different Types of Text
 
-        var t = new h2d.TextInput(hxd.res.DefaultFont.get(), s2d);
-        t.scale(10);
-        t.y = 200;
-        t.text = "Input text";
-    }
+        // Text 
+        // https://heaps.io/api/h2d/Text.html
+		var t = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
+		t.scale(10);
+		t.text = "Haxe Rocks!!!";
+
+        // HtmlText 
+        // https://heaps.io/api/h2d/HtmlText.html
+		var t = new h2d.HtmlText(hxd.res.DefaultFont.get(), s2d);
+		t.scale(10);
+		t.y = 100;
+		t.text = "Haxe <font color='#ff0000'>Rocks!!!</font>";
+
+        // TextInput
+        // https://heaps.io/api/h2d/TextInput.html
+		var t = new h2d.TextInput(hxd.res.DefaultFont.get(), s2d);
+		t.scale(10);
+		t.y = 200;
+		t.text = "Input text";
+	}
 }
