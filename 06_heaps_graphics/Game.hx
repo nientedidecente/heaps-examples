9e7165eef2e310223ab5043fb4ea4b0a9d5abe54
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-05-29 21:04:25 +0100

    translate example 6

--- 06_heaps_graphics/Game.hx
+++ 06_heaps_graphics/Game.hx
@@ -1,30 +1,28 @@
-class Game extends hxd.App
-{
-    static function main()
-    {
-        hxd.Res.initLocal();
-        
-        new Game();
-    }
+class Game extends hxd.App {
+	static function main() {
+		hxd.Res.initLocal();
 
-    var g:h2d.Graphics;
+		new Game();
+	}
 
-    override function init() 
-    {
-        g = new h2d.Graphics(s2d);
-        g.beginFill(0xff00ff, 0.5);
-        g.drawCircle(150, 150, 100);
-    }
+	var g:h2d.Graphics;
 
-    override function update(dt:Float) 
-    {
-        if (hxd.Key.isDown(hxd.Key.RIGHT))
-        {
-            g.x++;
-        }
-        else if (hxd.Key.isDown(hxd.Key.LEFT))
-        {
-            g.x--;
-        }
-    }
+	override function init() {
+		// Graphics https://heaps.io/api/h2d/Graphics.html
+		g = new h2d.Graphics(s2d);
+		g.beginFill(0xff00ff, 0.5);
+		// as the method suggests, we are drawing a circle of 100 pixel radius
+		g.drawCircle(150, 150, 100);
+	}
+
+	override function update(dt:Float) {
+		// binding RIGHT key to a positive x axis movement
+		if (hxd.Key.isDown(hxd.Key.RIGHT)) {
+			g.x += 10;
+		}
+		// binding LEFT key to a negative x axis movement
+		else if (hxd.Key.isDown(hxd.Key.LEFT)) {
+			g.x -= 10;
+		}
+	}
 }
