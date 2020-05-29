2132b0b6d5ebf1e52091d3ad679b95c99fdb581c
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-05-29 23:07:47 +0100

    translated example 10

--- 10_heaps_load_model/Game.hx
+++ 10_heaps_load_model/Game.hx
@@ -1,40 +1,36 @@
-class Game extends hxd.App
-{
-    static function main()
-    {
-        hxd.Res.initLocal();
-        
-        new Game();
-    }
+class Game extends hxd.App {
+	static function main() {
+		hxd.Res.initLocal();
 
-    var obj:h3d.scene.Object;
+		new Game();
+	}
 
-    override function init() 
-    {
-        // Загружаем модель и добавляем объект на сцену
-        var cache = new h3d.prim.ModelCache();
-        obj = cache.loadModel(hxd.Res.Model);
-        obj.scale(0.05);
-        obj.rotate(0, 0, Math.PI);
-        s3d.addChild(obj);
+	var obj:h3d.scene.Object;
 
-        // Загружаем анимацию, зашитую в Model.fbx
-        var anim = cache.loadAnimation(hxd.Res.Model);
-        // Воспроизводим анимацию
-        // Обратите внимание, что одну и ту же анимацию
-        // можно применять к разным моделям
-        obj.playAnimation(anim);
+	override function init() {
+		// Load the model and add it to the scene
+		var cache = new h3d.prim.ModelCache();
+		obj = cache.loadModel(hxd.Res.Model);
+		obj.scale(0.05);
+		obj.rotate(0, 0, Math.PI);
+		s3d.addChild(obj);
 
-        // Настраиваем освещение на сцене
-        var light = new h3d.scene.DirLight(new h3d.Vector( 0.3, -0.4, -0.9), s3d);
+		// Load the animation wired to the Model
+		var anim = cache.loadAnimation(hxd.Res.Model);
+        // play the animation.
+        // note that the same anim could be applied to different objects
+		obj.playAnimation(anim);
+
+		// setup the lights on the stage
+		var light = new h3d.scene.DirLight(new h3d.Vector(0.3, -0.4, -0.9), s3d);
 		light.enableSpecular = true;
 		light.color.set(0.28, 0.28, 0.28);
 		s3d.lightSystem.ambientLight.set(0.74, 0.74, 0.74);
 
-        // Создаем простой контроллер для камеры,
-        // чтобы ей можно было управлять мышью
-        new h3d.scene.CameraController(s3d).loadFromCamera();
+        // Create a simple CameraController
+        // so it can be driven by the mouse
+		new h3d.scene.CameraController(s3d).loadFromCamera();
 
-        engine.backgroundColor = 0xFF808080;
-    }
+		engine.backgroundColor = 0xFF808080;
+	}
 }
