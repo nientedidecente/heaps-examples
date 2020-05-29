7cc124acb4e7c857e9b518ef628d63d3e6381d36
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-05-29 22:50:06 +0100

    translated example 8

--- 08_heaps_shader/Game.hx
+++ 08_heaps_shader/Game.hx
@@ -1,78 +1,55 @@
-class MyShader extends hxsl.Shader
-{
-    static var SRC = {
-        
-        // Шейдер, от которого мы наследуемся.
-        // Из него мы имеем доступ ко всем его 
-        // аттрибутам и константам
-        @:import h3d.shader.Base2d;
-
-        // Определяем параметр шейдера, 
-        // к которому можно будет обратиться из кода
-        @param var color:Vec4;
-        
-        // Простейший фрагментный шейдер,
-        // в котором мы переопределяем цвет на выходе
-        function fragment()
-        {
-            output.color = color;
-        }
-    };
+class MyShader extends hxsl.Shader {
+	static var SRC = {
+		// Base Shader we inherit from
+		@:import h3d.shader.Base2d;
+		// this will be used to define the shaders params
+		@param var color:Vec4;
+		// A simple fragment shader where we override the output colour
+		function fragment() {
+			output.color = color;
+		}
+	};
 }
 
-class MyShader2 extends hxsl.Shader
-{
-    static var SRC = {
-        
-        @:import h3d.shader.Base2d;
-
-        @param var color:Vec4;
-        
-        function fragment()
-        {
-            output.color += color;
-        }
-    };
+class MyShader2 extends hxsl.Shader {
+	static var SRC = {
+		@:import h3d.shader.Base2d;
+		@param var color:Vec4;
+		function fragment() {
+			output.color += color;
+		}
+	};
 }
 
-class Game extends hxd.App
-{
-    static function main()
-    {
-        hxd.Res.initLocal();
-        
-        new Game();
-    }
+class Game extends hxd.App {
+	static function main() {
+		hxd.Res.initLocal();
 
-    var b:h2d.Bitmap;
+		new Game();
+	}
 
-    override function init() 
-    {
-        b = new h2d.Bitmap(hxd.Res.haxeLogo.toTile(), s2d);
-        var s = new MyShader();
-        // задаем значение параметра шейдера, 
-        // который мы добавили ранее
-        s.color.set(1.0, 0.0, 0.0, 1.0);
-        b.addShader(s);
+	var b:h2d.Bitmap;
 
-        // Добавляем объекту второй шейдер
-        var s2 = new MyShader2();
-        s2.color.set(0.0, 0.0, 1.0, 0.0);
+	override function init() {
+		b = new h2d.Bitmap(hxd.Res.haxeLogo.toTile(), s2d);
+		var s = new MyShader();
+		// setting the value of the shader param which we add on line 6
+		s.color.set(1.0, 0.0, 0.0, 1.0);
+		b.addShader(s);
+
+        // adding the second shader to the object
+		var s2 = new MyShader2();
+		s2.color.set(0.0, 0.0, 1.0, 0.0);
         b.addShader(s2);
 
-        b.x = 250;
+		b.x = 250;
         b.y = 250;
-        // центрируем тайл (задаем ему якорную точку),
-        // таким образом все трансформации объекта на экране 
-        // будут рассчитываться относительно центра тайла.
-        // Если данный метод не вызывать, то тайл будет трансформироваться
-        // относительно его левой верхней точки
-        b.tile = b.tile.center();
-        b.rotation = Math.PI / 4; // повороты задаются в радианах
-    }
+        
+		b.tile = b.tile.center();
+		b.rotation = Math.PI / 4;
+	}
 
-    override function update(dt:Float) 
-    {
-        b.rotation += 0.01;
-    }
+	override function update(dt:Float) {
+		b.rotation += 0.01;
+	}
 }
