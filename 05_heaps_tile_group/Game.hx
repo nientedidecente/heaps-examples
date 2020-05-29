b495bfe7eb72d3ce3cbceea448521b9f2c2399b3
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-05-29 20:58:10 +0100

    translated example 5

--- 05_heaps_tile_group/Game.hx
+++ 05_heaps_tile_group/Game.hx
@@ -1,31 +1,26 @@
-class Game extends hxd.App
-{
-    static function main()
-    {
-        hxd.Res.initLocal();
-        
-        new Game();
-    }
+class Game extends hxd.App {
+	static function main() {
+		hxd.Res.initLocal();
+		new Game();
+	}
 
-    var g:h2d.TileGroup;
+	var g:h2d.TileGroup;
 
-    override function init() 
-    {
-        // Разбиваем текстуру на кадры по сетке
-        var tiles = hxd.Res.haxeLogo.toTile().gridFlatten(10, 0, 0);
-        // создаем группу
-        g = new h2d.TileGroup(tiles[0], s2d);
-        // отключим blend mode для группы, это ускорит отрисовку тайлов
-        g.blendMode = None;
-        // и добавляем в нее тайлы, произвольно расположенные на экране
-        for (i in 0...1000)
-        {
-            g.add(Std.random(s2d.width), Std.random(s2d.height), tiles[i % tiles.length]);
-        }
-    }
+	override function init() {
+		// gridFlatten will split a tile into a list of tiles 10x10
+		// https://heaps.io/api/h2d/Tile.html#gridFlatten
+		var tiles = hxd.Res.haxeLogo.toTile().gridFlatten(10, 0, 0);
+		// we create a TileGroup
+		g = new h2d.TileGroup(tiles[0], s2d);
+		// we disable blend mode (for performance purposes)
+		g.blendMode = None;
+		// and then we add to it other radom pieces of the initially split tile
+		for (i in 0...1000) {
+			g.add(Std.random(s2d.width), Std.random(s2d.height), tiles[i % tiles.length]);
+		}
+	}
 
-    override function update(dt:Float) 
-    {
-        g.rotation += 0.01;
-    }
+	override function update(dt:Float) {
+		g.rotation += 0.01;
+	}
 }
