f2a911f24b6a8cb1a658ae5828ff80493ed34059
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-05-29 17:20:30 +0100

    translated first example

--- 01_heaps_color_tile/Game.hx
+++ 01_heaps_color_tile/Game.hx
@@ -16,13 +16,11 @@ class Game extends hxd.App
         b = new h2d.Bitmap(h2d.Tile.fromColor(0xff0000, 60, 60), s2d);
         b.x = 50;
         b.y = 100;
-        // центрируем тайл (задаем ему якорную точку),
-        // таким образом все трансформации объекта на экране 
-        // будут рассчитываться относительно центра тайла.
-        // Если данный метод не вызывать, то тайл будет трансформироваться
-        // относительно его левой верхней точки
+        // calling .center on the tile will set an anchor point on the tile center.
+        // All transformations operating on the tile will be calculated relatively its anchor.
+        // By default the anchor point its set to be on the upper left corner.
         b.tile = b.tile.center();
-        b.rotation = Math.PI / 4; // повороты задаются в радианах
+        b.rotation = Math.PI / 4; // Rotation values needs to be provided in Radians
     }
 
     override function update(dt:Float) 
