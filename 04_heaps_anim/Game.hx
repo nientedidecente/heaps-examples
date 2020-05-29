5230fb8f4b9dc3d8af52494cea1a5af1edc01430
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-05-29 18:08:17 +0100

    translated example 4

--- 04_heaps_anim/Game.hx
+++ 04_heaps_anim/Game.hx
@@ -1,19 +1,18 @@
-class Game extends hxd.App
-{
-    static function main()
-    {
-        hxd.Res.initLocal();
-        
-        new Game();
-    }
+class Game extends hxd.App {
+	static function main() {
+		hxd.Res.initLocal();
 
-    var b:h2d.Anim;
+		new Game();
+	}
 
-    override function init() 
-    {
-        var animationSpeed = 15;
-        b = new h2d.Anim(hxd.Res.haxeLogo.toTile().split(10, false), animationSpeed, s2d);
-        b.x = 250;
-        b.y = 250;
-    }
+	var b:h2d.Anim;
+
+	override function init() {
+		var animationSpeed = 15;
+		// Animations accepts an array of tiles
+		// .split() will return an array of tiles,in this example cutting them every 10 pixel
+		b = new h2d.Anim(hxd.Res.haxeLogo.toTile().split(10, false), animationSpeed, s2d);
+		b.x = 250;
+		b.y = 250;
+	}
 }
