020d45c21eea73cd48a4de6a871db7604f349567
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-05-31 15:44:11 +0100

    fixed and translated example 18

--- 18_heaps_scaleGrid/Game.hx
+++ 18_heaps_scaleGrid/Game.hx
@@ -4,28 +4,24 @@ import hxd.App;
 
 /**
  * ScaleGrid usage example.
- * ScaleGrid class implements https://en.wikipedia.org/wiki/9-slice_scaling for Heaps.
- * Asset taken from Kenney's asset pack: https://opengameart.org/content/pixel-ui-pack-750-assets
+ * https://heaps.io/api/h2d/ScaleGrid.html
+ * ScaleGrid class implements https://en.wikipedia.org/wiki/9-slice_scaling scaling technique in Heaps.
+ * The asset image was taken from Kenney's asset pack: https://opengameart.org/content/pixel-ui-pack-750-assets
  */
-class Game extends hxd.App
-{
-    static function main()
-    {
-        hxd.Res.initEmbed();
-        
-        new Game();
-    }
+class Game extends hxd.App {
+	static function main() {
+		hxd.Res.initEmbed();
 
-    override function init() 
-    {
-        var tile = hxd.Res.load("space.png").toImage().toTile();
+		new Game();
+	}
 
-        // first argument is tile which will be sliced and tiled
-        // second argument is border width, which is the same on the left and on the right
-        // third argument is border height, which is the same on the top and on the bottom
-        var scaleGrid = new h2d.ScaleGrid(tile, 2, 2, s2d);
-        scaleGrid.width = 300;
-        scaleGrid.height = 200;
-        scaleGrid.x = scaleGrid.y = 100;
-    }
+	override function init() {
+		var tile = hxd.Res.load("space.png").toImage().toTile();
+
+		// First argument is tile itself, second argument is border width, the third argument is border height
+		var scaleGrid = new h2d.ScaleGrid(tile, 2, 2, s2d);
+		scaleGrid.width = 300;
+		scaleGrid.height = 200;
+		scaleGrid.x = scaleGrid.y = 100;
+	}
 }
