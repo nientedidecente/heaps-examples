23e0f4d798c783f2b3b87655eed97da863f451c3
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-05-31 16:41:08 +0100

    fixed and documented example 23

--- 22_heaps_flambe_behaviors/Game.hx
+++ 22_heaps_flambe_behaviors/Game.hx
@@ -1,55 +1,60 @@
+// this example contains modified code from the Flambe game engine: https://github.com/aduros/flambe
+class Game extends hxd.App {
+	static function main() {
+		hxd.Res.initLocal();
 
-// this example contains modified code from Flambe game engine: https://github.com/aduros/flambe
-class Game extends hxd.App
-{
-    static function main()
-    {
-        hxd.Res.initLocal();
-        
-        new Game();
-    }
+		new Game();
+	}
 
-    var b:h2d.Bitmap;
+	var b:h2d.Bitmap;
 
-    var script:flambe.script.Script;
+	var script:flambe.script.Script;
 
-    override function init() 
-    {
-        b = new h2d.Bitmap(hxd.Res.haxeLogo.toTile(), s2d);
-        b.x = 250;
-        b.y = 250;
-        
-        // sequence of actions
-        var sequence = new flambe.script.Sequence([
-            // first action is just delay for 2.5 seconds
-            new flambe.script.Delay(2.5),
-            // then we just set color to red
-            new flambe.script.CallFunction(function() { b.color = new h3d.Vector(1.0, 0.0, 0.0, 1.0); }),
-            // then again delay for another 3.5 seconds
-            new flambe.script.Delay(3.5),
-            // and set color to blue
-            new flambe.script.CallFunction(function() { b.color = new h3d.Vector(0.0, 0.0, 1.0, 1.0); }),
-            // wait another 1.5 seconds
-            new flambe.script.Delay(1.5),
-            // and set color to normal
-            new flambe.script.CallFunction(function() { b.color = new h3d.Vector(1.0, 1.0, 1.0, 1.0); })
-        ]);
+	override function init() {
+		b = new h2d.Bitmap(hxd.Res.haxeLogo.toTile(), s2d);
+		b.x = 250;
+		b.y = 250;
 
-        // let's repeat this sequence forever (-1 means forever)
-        var repeat = new flambe.script.Repeat(sequence, -1);
+		// Flambe script Sequence will play a sequence of functions/actions
+		var sequence = new flambe.script.Sequence([
+			// the first action is just a delay of 1.5 seconds
+			new flambe.script.Delay(1.5),
+			// then we just set the icon colour to red
+			new flambe.script.CallFunction(function() {
+				// a 3 dimensional vector is another way of setting the RGBa value
+				b.color = new h3d.Vector(1.0, 0.0, 0.0, 1.0);
+			}),
+			// then again  we wait for another 2.5 seconds
+			new flambe.script.Delay(2.5),
+			// and set the colour to blue
+			new flambe.script.CallFunction(function() {
+				b.color = new h3d.Vector(0.0, 0.0, 1.0, 1.0);
+			}),
+			// we wait another 2.5 seconds
+			new flambe.script.Delay(2.5),
+			// and set colour back to normal
+			new flambe.script.CallFunction(function() {
+				b.color = new h3d.Vector(1.0, 1.0, 1.0, 1.0);
+			})
+		]);
 
-        // and create script object, which will run our repeat action
+		// to repeat this sequence forever we just call this function specifying -1
+		var repeat = new flambe.script.Repeat(sequence, -1);
+
+		// finally to run our ssequence we need to create a script object,
         script = new flambe.script.Script();
-        script.run(repeat);
+        // and pass it out repeat sequence
+		script.run(repeat);
 
         // you want to execute this sequence just one time, then you could just call script.run(sequence);
-    }
+        // as by default the second parameter is 1
+	}
 
-    override function update(dt:Float) 
-    {
-        // update script object
-        script.update(hxd.Timer.elapsedTime);
+	override function update(dt:Float) {
+        // on the update functin we will need to specify the elapsed time
+        // so the script will know what to do
+		script.update(hxd.Timer.elapsedTime);
 
-        // if you'll want to stop this script, then you can just call script.stopAll() method.
-    }
+		// if you want to stop this script, you can just call script.stopAll() method.
+	}
 }
